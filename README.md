42bfbabfbe6ed59daaf74696d3951ac3bf3e01f3
Bijon Saha <bijon176193@gmail.com>
2025-09-27 11:23:00 +0600

    Task 3 Done

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Task-Manager
+The outputs of all tasks of Task Manager assignment. 
 
 ## Task 1 Output: 
 Data Types and Variables
@@ -24,4 +25,40 @@ Task {
   dueDate: 2023-12-31T00:00:00.000Z,
   category: 'School'
 }
+```
+## Task 3 : Functions
+- Output of createTask function:
+This function is used to create new tasks for a user. 
+```Created Tasks: [
+  Task {
+    id: '131028 + 1758948049351',
+    description: 'Description 1',
+    priority: 3,
+    completed: true,
+    createdDate: 2025-09-27T04:40:49.351Z,
+    dueDate: 2023-12-31T00:00:00.000Z,
+    category: 'Work'
+  },
+  Task {
+    id: '131028 + 1758948049351',
+    description: 'Description 2',
+    priority: 3,
+    completed: true,
+    createdDate: 2025-09-27T04:40:49.351Z,
+    dueDate: 2023-11-30T00:00:00.000Z,
+    category: 'Personal'
+  }
+]
+```
+- Output of deleteTask function: 
+This function is work for delete all tasks by using its id. To run this function, it will delete all existing tasks of a id.
+```
+After Deletion: []
+```
+
+- Display Tasks by category: 
+This function mainly shows all the tasks of same category.
+```
+Displaying tasks by category: Work
+ID: 131028 + 1758950272537, Title: Task 1, Due: Sun Dec 31 2023, Completed: false
 ```
--- index.js
+++ index.js
@@ -1,28 +1,67 @@
 //Task 1: information - Data types and variables
 const studentInformation = {
-    id: 131028,
-    name: "Bijon Saha",
-    birthMonth: 4,
-    favoriteColor: "Black"
-}
+  id: 131028,
+  name: "Bijon Saha",
+  birthMonth: 4,
+  favoriteColor: "Black",
+};
 
-console.log(studentInformation);
+//console.log(studentInformation);
 
 //Task 2: Task object constructor
 
-class Task{
-    constructor(title, description, dueDate,category,priority = 3){
-        this.id = `${studentInformation.id} + ${Date.now()}`;
-        this,title = title;
-        this.description = description;
-        this.priority = priority;
-        this.completed = true;
-        this.createdDate = new Date();
-        this.dueDate = new Date(dueDate);
-        this.category = category;
-
-    }
+class Task {
+  constructor(title, description, dueDate, category, priority = 3) {
+    this.id = `${studentInformation.id} + ${Date.now()}`;
+    this.title = title;
+    this.description = description;
+    this.priority = priority;
+    this.completed = false;
+    this.createdDate = new Date();
+    this.dueDate = new Date(dueDate);
+    this.category = category;
+  }
 }
 
-const demo1 = new Task("Bijon's Task Manager", "This is an assignment", "2023-12-31", "School", 1);
-console.log(demo1);
+const t1 = new Task(
+  "Bijon's Task Manager",
+  "This is an assignment",
+  "2023-12-31",
+  "School",
+  1
+);
+//console.log(demo1);
+
+//Task 3 : Functions
+let tasks = [];
+function createTask(title, description, dueDate, category) {
+  let newTask = new Task(title, description, dueDate, category);
+  tasks.push(newTask);
+  return newTask;
+}
+createTask("Task 1", "Description 1", "2023-12-31", "Work");
+createTask("Task 2", "Description 2", "2023-11-30", "Personal");
+//console.log("Created Tasks:", tasks);
+
+// Delete task by id
+function deleteTask(id){
+    tasks = tasks.filter(task => task.id !== id);
+    return tasks;
+}
+//console.log("After Deletion:", deleteTask(t1.id));
+
+// display all tasks optionally filter by category
+function displayTasks(category){
+  console.log(`Displaying tasks by category: ${category}`);
+  const filteredTasks = tasks.filter(task => task.category === category);
+
+  if(filteredTasks.length === 0){
+    console.log("No tasks found in this category.");
+  } else {
+    filteredTasks.forEach(task => {
+      console.log(`ID: ${task.id}, Title: ${task.title}, Due: ${task.dueDate.toDateString()}, Completed: ${task.completed}`);
+    });
+  }
+}
+displayTasks("Work");
+
